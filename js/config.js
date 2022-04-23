025a1d750a668973900f646d8a7740546ad5ce26
uniq <93861631+un1qs@users.noreply.github.com>
2022-04-24 10:39:37 +1200

    Файлы сайта

---
+++ js/config.js
@@ -0,0 +1,45 @@
+let config = {
+
+  // Задержка загрузки (1 секунда)
+  preloaderDelay: 1,
+
+  // Задержка появления видео (4.2 секунды)
+  showDelay: 2.5, 
+
+  // Заголовок
+  title: 'unfeel#0001',
+
+  // Социальные сети
+  social: {
+    telegram: '@unfeelmeow',
+    steam: 'DarKi',
+    vk: 'unfeel.here', 
+  },
+
+
+  // Цвет иконки загрузки
+  preloaderColor: '#ff0000',
+
+
+  // Цвет кнопки play
+  playButtonColor: '#ff0000',
+  // Цвет тени кнопки play
+  playButtonShadowColor: 'rgba(255, 0, 55, 5.0)',
+
+
+  // Цвет заголовка
+  titleColor: '#FF0000',
+  // Цвет тени загаловка
+  titleShadowColor: 'rgba(255, 0, 55, 0.6)',
+
+
+  // Текст копирайта
+  copyrightText: 'dev - unfeel#0001',
+  // Цвет копирайта
+  copyrightTextColor: '#f5323c',
+
+  // Цвет иконок социальных сетей
+  socialColor: '#FF0000',
+  // Цвет тени иконок социальных сетей
+  socialShadowColor: 'rgba(255, 255, 255, 0.2)',
+}
